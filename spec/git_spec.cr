cc56ab88c5b10c6149fce115a130361f89e0235b
Caspian Baska <email@caspian.computer>
2021-09-09 18:27:58 +1000

    fix: typo

--- spec/git_spec.cr
+++ spec/git_spec.cr
@@ -149,7 +149,7 @@ module PlaceOS::Compiler
       it "will check out a file, then restore to a repo state where it does not exist" do
         file = "drivers/aca/private_helper.cr"
         path = File.join(repository_path, file)
-        File.exists?(repository_path).should be_false
+        File.exists?(path).should be_false
 
         Git.checkout_file(file, repository, working_directory, commit: "9e5c982e2aac06e1e10349fe8352109c138d7f23") do
           File.exists?(path).should be_true
